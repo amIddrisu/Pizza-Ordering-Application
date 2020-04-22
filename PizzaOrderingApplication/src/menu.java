b6fb6a3441ad080ec932f1e16f3cc6755f4e68fd
amIddrisu <abdulmanan32@gmail.com>
2020-04-22 00:53:26 +0000

    Added Pizza class and updated Menu class

--- PizzaOrderingApplication/src/menu.java
+++ PizzaOrderingApplication/src/menu.java
@@ -1,8 +1,12 @@
 
 public class menu {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		pizza pz = new pizza();
+		
 
 	}
 
---
+++ PizzaOrderingApplication/src/pizza.java
@@ -0,0 +1,36 @@
+
+public class pizza {
+	//creating an instance variable for pizza
+	String name;
+	String size;
+	int price;
+	
+	//getters 
+	public String getName() {
+		return name;
+	}
+	
+	public String getSize() {
+		return size;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	
+	
+	//setters
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setSize(String size) {
+		this.size = size;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
